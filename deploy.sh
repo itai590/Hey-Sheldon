0aaf94c89411e1231083adcdffbfd3f15e8a1f29
Itai Koren <itai.koren@psifas.org.il>
2025-04-26 20:40:06 +0300

    feat(deploy): clean up old containers before building and deploying

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,20 @@ set -e
 TAG=$(date +"%d%b%y_%H%M")
 export TAG=$TAG
 
-echo "🚀 Building and deploying hey-sheldon with tag $TAG..."
+echo "🧹 Cleaning old containers if exist..."
+docker rm -f hey-sheldon-server || true
+docker rm -f hey-sheldon-client || true
 
 echo "🛑 Stopping old containers..."
 docker compose down
 
-echo "🏗 Building and Starting containers..."
+echo "🏗 Building containers..."
 docker compose build
+
+echo " 🏷️ Tagging hey-sheldon-server image with tag $TAG..."
 docker tag hey-sheldon-server:latest hey-sheldon-server:"$TAG"
+
+echo "🚀 Starting containers..."
 docker compose up -d
 
 echo "📜 Showing live logs..."
