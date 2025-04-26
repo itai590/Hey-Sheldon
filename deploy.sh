ff3d669b7774c0013279e54461b2dd6c9f8aeaa0
Itai Koren <itai.koren@psifas.org.il>
2025-04-26 18:40:04 +0300

    fix(deploy): update CONFIG_PATH for Linux environment
    Corrected the CONFIG_PATH for the Linux environment in the deploy script to point to the correct directory. This change ensures that the script references the proper path for the Hey-Sheldon project, preventing potential deployment issues on Linux systems.

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "🚀 Building and deploying hey-sheldon ..."
 UNAME_OUT="$(uname -s)"
 
 case "${UNAME_OUT}" in
-    Linux*)  export CONFIG_PATH="/home/pi/Sheldon/Hey-Sheldon/server/config.json";;
+    Linux*)  export CONFIG_PATH="/home/pi/Hey-Sheldon/server/config.json";;
     Darwin*) export CONFIG_PATH="$HOME/Documents/Offline-Repos/Hey-Sheldon/server/config.json";;
     *)       echo "❌ Unsupported OS: ${UNAME_OUT}" && exit 1;;
 esac
