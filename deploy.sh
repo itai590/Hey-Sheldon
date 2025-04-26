f3bad4198e43a00779cb8a833567c54d57070866
Itai Koren <itai.koren@psifas.org.il>
2025-04-26 20:20:57 +0300

    fix(deploy): quote TAG variable in build argument for Docker compose

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ echo "🛑 Stopping old containers..."
 docker compose down
 
 echo "🏗 Building and Starting containers..."
-docker compose build --build-arg TAG=$TAG
+docker compose build --build-arg TAG="$TAG"
 docker compose up -d
 
 echo "📜 Showing live logs..."
