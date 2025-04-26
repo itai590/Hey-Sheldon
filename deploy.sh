e56acf31960b4f9f88eebea99fb4ee354eec7732
Itai Koren <itai.koren@psifas.org.il>
2025-04-26 23:40:58 +0300

    feat(deploy): build client and server containers separately during deployment

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,8 @@ echo -e "\n🛑 Stopping old containers..."
 docker compose down
 
 echo -e "\n🏗 Building containers..."
-docker compose build --no-cache
+docker compose build server
+docker compose build --no-cache client
 
 
 echo -e "\n🏷️ Tagging hey-sheldon-server image with tag $TAG..."
